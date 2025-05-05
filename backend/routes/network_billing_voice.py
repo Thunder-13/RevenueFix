0a7ce7857a9d293359ae691713058aba68cef705
Abinesh18-HP <abinesh.gunasekaran@accenture.com>
2025-05-05 19:57:27 +0530

    csv files for Voice/SMS

---
+++ backend/routes/network_billing_voice.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify
+from models.services import ServicesModel
+ 
+network_billing_voice_bp = Blueprint('network_billing_voice', __name__)
+ 
+@network_billing_voice_bp.route('', methods=['GET'])
+def get_network_billing_voice():
+    """Get Network vs Billing Voice"""
+    try:
+       
+        return jsonify({
+            'status': 'success',
+            'data': ServicesModel.get_network_vs_billing_voice()
+        })
+       
+    except Exception as e:
+        print(f"Error processing Network vs Billing Voice: {e}")
+        return jsonify({
+            'status': 'error',
+            'message': str(e)
+        }), 500
